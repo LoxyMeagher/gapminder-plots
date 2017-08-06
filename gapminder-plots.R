e6053afcb0ea5e458a68d04f768df73dbfedbf41
Kristin Meagher Robinson <krobinso@stanford.edu>
2017-08-06 12:00:46 -0700

    fix #2: made graph for Japan's life expectancy :jp:

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -6,3 +6,10 @@ library(ggplot2)
 
 ggplot(gapminder, aes(y = lifeExp, x = gdpPercap)) +
   geom_point() +geom_smooth(method = "lm", color = "red")
+
+
+japan_gapminder <- gapminder[gapminder$country == "Japan",]
+
+ggplot(japan_gapminder, aes(x = year, y = lifeExp)) + 
+  geom_line(color = "blue")+ geom_point(color = "blue") +
+  xlab("Life expectancy") + ylab("Year")
